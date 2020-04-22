ea012b1f256cb3f7974a0ffd7cb930eab4f70951
AleksandrPuhov <aleksandr_puhov@list.ru>
2020-04-22 23:30:22 +0300

    start of change

---
+++ app/js/main.js
@@ -0,0 +1,41 @@
+// const btnRicardo = document.getElementById('btn-ricardo');
+// const btnAlberto = document.getElementById('btn-alberto');
+// const btnThomas = document.getElementById('btn-thomas');
+
+// btnRicardo.onclick = () => {
+//     const aRicardo1 = document.getElementById('a-ricardo-1');
+//     const aRicardo2 = document.getElementById('a-ricardo-2');
+//     const aRicardo3 = document.getElementById('a-ricardo-3');
+//     const aRicardo4 = document.getElementById('a-ricardo-4');
+
+//     aRicardo1.hidden = false;
+//     aRicardo2.hidden = false;
+//     aRicardo3.hidden = false;
+//     aRicardo4.hidden = false;
+//     btnRicardo.hidden = true;
+// };
+
+// btnAlberto.onclick = () => {
+//     const aAlberto1 = document.getElementById('a-alberto-1');
+//     const aAlberto2 = document.getElementById('a-alberto-2');
+//     const aAlberto3 = document.getElementById('a-alberto-3');
+//     const aAlberto4 = document.getElementById('a-alberto-4');
+
+//     aAlberto1.hidden = false;
+//     aAlberto2.hidden = false;
+//     aAlberto3.hidden = false;
+//     aAlberto4.hidden = false;
+//     btnAlberto.hidden = true;
+// };
+// btnThomas.onclick = () => {
+//     const aThomas1 = document.getElementById('a-thomas-1');
+//     const aThomas2 = document.getElementById('a-thomas-2');
+//     const aThomas3 = document.getElementById('a-thomas-3');
+//     const aThomas4 = document.getElementById('a-thomas-4');
+
+//     aThomas1.hidden = false;
+//     aThomas2.hidden = false;
+//     aThomas3.hidden = false;
+//     aThomas4.hidden = false;
+//     btnThomas.hidden = true;
+// };
