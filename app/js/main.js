716fc039db4fe821b92810987795e58ba3dc7f11
AleksandrPuhov <aleksandr_puhov@list.ru>
2020-06-06 01:25:28 +0300

    work with mobile adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,25 @@
 $(document).ready(function () {
     const navbar = $("#navbar");
+    const navbarBurger = $("#navbar-burger");
+    const navbarActiv = "header-navbar-activ";
+
+    const btnRicardo = $("#btn-ricardo");
+    const btnAlberto = $("#btn-alberto");
+    const btnThomas = $("#btn-thomas");
+
+    btnRicardo.click(function () {
+        btnRicardo.hide();
+        $("#i-ricardo-1, #i-ricardo-2, #i-ricardo-3, #i-ricardo-4").show();
+    });
+
+    btnAlberto.click(function () {
+        $("#i-alberto-1, #i-alberto-2, #i-alberto-3, #i-alberto-4").show();
+        btnAlberto.hide();
+    });
+    btnThomas.click(function () {
+        $("#i-thomas-1, #i-thomas-2, #i-thomas-3, #i-thomas-4").show();
+        btnThomas.hide();
+    });
 
     $(window).scroll(function () {
         const scrolled = $(this).scrollTop();
@@ -10,46 +30,9 @@ $(document).ready(function () {
             navbar.css({ backgroundColor: "transparent" });
         }
     });
+
+    navbarBurger.click(function () {
+        navbar.toggleClass(navbarActiv);
+        $("body").toggleClass("lock");
+    });
 });
-
-// const btnRicardo = document.getElementById('btn-ricardo');
-// const btnAlberto = document.getElementById('btn-alberto');
-// const btnThomas = document.getElementById('btn-thomas');
-
-// btnRicardo.onclick = () => {
-//     const aRicardo1 = document.getElementById('a-ricardo-1');
-//     const aRicardo2 = document.getElementById('a-ricardo-2');
-//     const aRicardo3 = document.getElementById('a-ricardo-3');
-//     const aRicardo4 = document.getElementById('a-ricardo-4');
-
-//     aRicardo1.hidden = false;
-//     aRicardo2.hidden = false;
-//     aRicardo3.hidden = false;
-//     aRicardo4.hidden = false;
-//     btnRicardo.hidden = true;
-// };
-
-// btnAlberto.onclick = () => {
-//     const aAlberto1 = document.getElementById('a-alberto-1');
-//     const aAlberto2 = document.getElementById('a-alberto-2');
-//     const aAlberto3 = document.getElementById('a-alberto-3');
-//     const aAlberto4 = document.getElementById('a-alberto-4');
-
-//     aAlberto1.hidden = false;
-//     aAlberto2.hidden = false;
-//     aAlberto3.hidden = false;
-//     aAlberto4.hidden = false;
-//     btnAlberto.hidden = true;
-// };
-// btnThomas.onclick = () => {
-//     const aThomas1 = document.getElementById('a-thomas-1');
-//     const aThomas2 = document.getElementById('a-thomas-2');
-//     const aThomas3 = document.getElementById('a-thomas-3');
-//     const aThomas4 = document.getElementById('a-thomas-4');
-
-//     aThomas1.hidden = false;
-//     aThomas2.hidden = false;
-//     aThomas3.hidden = false;
-//     aThomas4.hidden = false;
-//     btnThomas.hidden = true;
-// };
